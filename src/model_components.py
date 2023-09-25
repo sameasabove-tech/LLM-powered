27ac922c696d03253855236f28af655940608a00
justmeloic <loic.muhirwa@gmail.com>
2023-09-25 10:17:08 -0400

    update: model

---
+++ src/model_components.py
@@ -0,0 +1,21 @@
+
+from dataclasses import dataclass
+
+@dataclass
+class PromptInput:
+    """
+    
+    """
+    prompt: str
+    id: int = 1 # set id to 1, but change this to call the util function after 
+
+
+
+@dataclass
+class PromptOutput:
+    """
+    
+    """
+    answer: str
+    id: int = 1
+
